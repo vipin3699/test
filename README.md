00839e44b668fbbf272718dd6cc901c94ba4de94
Kumar Vipin Yadav <kumarvipinyadav369@gmail.com>
2022-02-07 10:38:52 +0530

    Validating 2 Job in a single GitHub Action.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # test
+This is a test repo Used to test GitHub Actions
--- test_action.sh
+++ test_action.sh
@@ -1,1 +1,2 @@
 echo "This is a file use by GitHub actions to test the deployment. version 01"
+echo "It need the sh file so we have to use the actions/checkout@v2"
